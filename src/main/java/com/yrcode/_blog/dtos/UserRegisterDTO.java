3382cab704c78440939c505cd2343fec606650ff
yrahhaou <yassinerahhaoui12@gmail.com>
2026-02-27 01:56:38 +0000

    create UserRegisterDTO

--- src/main/java/com/yrcode/_blog/dtos/UserRegisterDTO.java
+++ src/main/java/com/yrcode/_blog/dtos/UserRegisterDTO.java
@@ -1,5 +1,30 @@
 package com.yrcode._blog.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record UserRegisterDTO(
-    
+
+    @NotBlank(message = "Username is required!")
+    @Size(min = 2, max = 60, message = "Username must be between 2 and 60 characters")
+    String username,
+
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Invalid email format")
+    @Pattern(
+        regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", 
+        message = "Email must be valid (e.g. user@example.com)"
+    )
+    String email,
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 8, message = "Password must be at least 8 characters") // 2. أقل حاجة 8 حروف
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", 
+             message = "Password must contain uppercase, lowercase, number and special character")
+    String password,
+
+    @NotBlank(message = "Password confirmation is required!")
+    String password_confirmation
 ) {}
