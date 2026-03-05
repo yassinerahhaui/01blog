e64857b6ec0017d76dd0bff2789230234df5b877
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-05 05:02:38 +0000

    create post entity

--- src/main/java/com/yrcode/_blog/entities/PostEntity.java
+++ src/main/java/com/yrcode/_blog/entities/PostEntity.java
@@ -1,10 +1,38 @@
 package com.yrcode._blog.entities;
 
+import java.util.UUID;
+
+import com.yrcode._blog.enums.MediaType;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name="posts")
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostEntity extends AbstractEntity {
+    @Column(name="user_id",nullable=false)
+    private UUID userId;
     
+    @Column(nullable=false)
+    private String content;
+    
+    @Column(nullable=true)
+    private String mediaUrl;
+
+    @Enumerated(EnumType.STRING)
+    @lombok.Builder.Default
+    private MediaType mediaType = MediaType.EMPTY;
 }
