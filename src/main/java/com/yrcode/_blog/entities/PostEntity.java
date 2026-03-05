c68acdb7255e1a9909499934d03341e588e6b12d
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-05 01:29:52 +0000

    create postEntity class is empty not completed

---
+++ src/main/java/com/yrcode/_blog/entities/PostEntity.java
@@ -0,0 +1,10 @@
+package com.yrcode._blog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="posts")
+public class PostEntity extends AbstractEntity {
+    
+}
