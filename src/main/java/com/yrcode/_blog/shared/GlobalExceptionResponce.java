3bd620489bb4f2566f82910129b0df8091cda832
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-05 02:16:41 +0000

    handle validations errors

--- src/main/java/com/yrcode/_blog/shared/GlobalExceptionResponce.java
+++ src/main/java/com/yrcode/_blog/shared/GlobalExceptionResponce.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -20,6 +21,14 @@ public class GlobalExceptionResponce {
     @ExceptionHandler(CustomResponseException.class)
     public ResponseEntity<GlobalResponse<?>> handleCustomException(CustomResponseException ex) {
         var errors = List.of(new GlobalResponse.ErrorItem(ex.getMessage()));
-        return new ResponseEntity<>(new GlobalResponse<>(errors),HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new GlobalResponse<>(errors),HttpStatus.resolve(ex.getCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GlobalResponse<?>> handleValidationException(MethodArgumentNotValidException ex) {
+        var errors = ex.getBindingResult().getFieldErrors().stream()
+            .map(err -> new GlobalResponse.ErrorItem(err.getField() + " " + err.getDefaultMessage()))
+            .toList();
+        return new ResponseEntity<>(new GlobalResponse<>(errors),HttpStatus.BAD_REQUEST);
     }
 }
