358e56c5ef987640749f4435d4f0e24da1972d47
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-07 14:19:52 +0000

    update postEntity

--- src/main/java/com/yrcode/_blog/entities/PostEntity.java
+++ src/main/java/com/yrcode/_blog/entities/PostEntity.java
@@ -2,12 +2,18 @@ package com.yrcode._blog.entities;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.yrcode._blog.enums.MediaType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,16 +29,24 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostEntity extends AbstractEntity {
-    // @Column(name="user_id",nullable=false)
-    // private UUID userId;
+
     
     @Column(nullable=false)
     private String content;
     
     @Column(nullable=true)
     private String mediaUrl;
-
+    
     @Enumerated(EnumType.STRING)
     @lombok.Builder.Default
     private MediaType mediaType = MediaType.EMPTY;
+    
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @JoinColumn(name="user_id",nullable=false)
+    @OnDelete(action=OnDeleteAction.CASCADE)
+    private UserEntity userId;
+
+    public UUID getUserId() {
+        return  userId.getId();
+    }
 }
