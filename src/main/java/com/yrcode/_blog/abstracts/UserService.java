24b8a015bbd9f57ef351b872502aa3e41c6896e0
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-08 14:49:21 +0000

    create UserService interface

---
+++ src/main/java/com/yrcode/_blog/abstracts/UserService.java
@@ -0,0 +1,16 @@
+package com.yrcode._blog.abstracts;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.yrcode._blog.dtos.UserDetailsDTO;
+import com.yrcode._blog.dtos.UserRegisterDTO;
+import com.yrcode._blog.entities.UserEntity;
+
+public interface UserService {
+    UserDetailsDTO findOne(UUID userId);
+    List<UserDetailsDTO> findAll();
+    UserDetailsDTO createOne(UserRegisterDTO user);
+    void deleteOne(UUID userId);
+    UserDetailsDTO updateOne(UserEntity user);
+}
