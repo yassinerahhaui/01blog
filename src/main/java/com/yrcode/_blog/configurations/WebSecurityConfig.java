b3b56d6b71cf094a087b2cd0e5c83d3b6ec1f0b8
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-10 15:41:26 +0000

    update configuration

--- src/main/java/com/yrcode/_blog/configurations/WebSecurityConfig.java
+++ src/main/java/com/yrcode/_blog/configurations/WebSecurityConfig.java
@@ -2,8 +2,10 @@ package com.yrcode._blog.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll()
+            .authorizeHttpRequests(req -> req.requestMatchers("/api/auth/**").permitAll()
             .anyRequest().authenticated());
             // .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             // .authenticationProvider(authenticationProvider);
