872e4591e05a98c0cbf159b3eaa0cbd8f2e089e4
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-20 00:40:50 +0000

    validate create post dto

--- src/main/java/com/yrcode/_blog/dtos/post/PostCreateDTO.java
+++ src/main/java/com/yrcode/_blog/dtos/post/PostCreateDTO.java
@@ -1,14 +1,26 @@
 package com.yrcode._blog.dtos.post;
 
+import org.hibernate.validator.constraints.URL;
+
 import com.yrcode._blog.enums.MediaType;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record PostCreateDTO(
-    @Size(max=5000,message="Your content is too long. Please limit it to 5,000 characters or less.")
+    @NotBlank(message = "Post content cannot be empty.")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9\\s_\\-]{10,500}$",
+        message = "Content can only contain letters, numbers, spaces, underscores (_), and dashes (-). Length must be 10-500 characters."
+    )
     String content,
+    @URL(message = "The media URL must be a valid and properly formatted link.")
+    @Size(max = 2083, message = "The media URL is too long.")
     String mediaUrl,
+    @NotNull(message = "Media type must be specified.")
     MediaType mediaType
 ) {}
