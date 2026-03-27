60ba42eea5b4470c59800cc2b35ca7af16f996cc
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-27 06:12:40 +0100

    create AuthController

--- src/main/java/com/yrcode/_blog/controllers/AuthController.java
+++ src/main/java/com/yrcode/_blog/controllers/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yrcode._blog.dtos.user.UserDetailsDTO;
+import com.yrcode._blog.dtos.user.UserLoginDTO;
 import com.yrcode._blog.dtos.user.UserRegisterDTO;
 import com.yrcode._blog.services.AuthService;
 import com.yrcode._blog.shared.GlobalResponse;
@@ -28,7 +29,8 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDetailsDTO> login(@RequestBody @Valid UserRegisterDTO userData) {
-        return null;
+    public ResponseEntity<String> login(@RequestBody @Valid UserLoginDTO userData) {
+        String token = authService.login(userData);
+        return ResponseEntity.status(HttpStatus.OK).body(token);
     }
 }
