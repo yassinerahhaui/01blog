f7005b8e08672600d85c9255ac0c8a068d257bbf
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-27 02:43:51 +0100

    add passwordEncoder method

--- src/main/java/com/yrcode/_blog/configurations/WebSecurityConfig.java
+++ src/main/java/com/yrcode/_blog/configurations/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,5 +27,10 @@ public class WebSecurityConfig {
             // .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             // .authenticationProvider(authenticationProvider);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
