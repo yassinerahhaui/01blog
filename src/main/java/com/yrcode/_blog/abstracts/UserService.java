5bbe8792345de0d3197d895d8376865c6bebd1a7
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-27 02:47:29 +0100

    update UserService

--- src/main/java/com/yrcode/_blog/abstracts/UserService.java
+++ src/main/java/com/yrcode/_blog/abstracts/UserService.java
@@ -4,13 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import com.yrcode._blog.dtos.user.UserDetailsDTO;
-import com.yrcode._blog.dtos.user.UserRegisterDTO;
 import com.yrcode._blog.dtos.user.UserUpdateDTO;
 
 public interface UserService {
     UserDetailsDTO findOne(UUID userId);
     List<UserDetailsDTO> findAll();
-    UserDetailsDTO createOne(UserRegisterDTO user);
     void deleteOne(UUID userId);
     UserDetailsDTO updateOne(UserUpdateDTO user);
 }
