49e5891d346e72fd4ed15315fc4978dff74cc933
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-29 07:40:09 +0100

    add navbar

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgbCollapseModule, NgbDropdownModule, CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  
+  public isMenuCollapsed = true;
+
+  onLogout() {
+    localStorage.removeItem('token');
+    console.log('Logged out successfully');
+  }
 }
