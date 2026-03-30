e39c7060371539c4bd857e02b489af41326e9b05
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-30 07:29:07 +0100

    create authInterceptor

---
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -0,0 +1,22 @@
+import { isPlatformBrowser } from '@angular/common';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const platformId = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(platformId)) {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const clonedRequest = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next(clonedRequest);
+    }
+  }
+
+  return next(req);
+};
