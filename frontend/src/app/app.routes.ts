6f9b04f8a072d08d59fd652012d1449603abae29
yrahhaou <yassinerahhaoui12@gmail.com>
2026-03-30 07:32:36 +0100

    add guards to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/auth/login/login';
 import { Register } from './pages/auth/register/register';
 import { NotFound } from './pages/not-found/not-found';
+import { authGuard } from './core/guards/auth-guard';
+import { notAuthGuard } from './core/guards/guest-guard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  { path: '', component: Home, canActivate: [authGuard] },
+  { path: 'login', component: Login, canActivate: [notAuthGuard] },
+  { path: 'register', component: Register, canActivate: [notAuthGuard] },
   { path: '**', component: NotFound },
 ];
