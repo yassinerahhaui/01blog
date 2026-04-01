1985ce18cb8a5f8d347de82b098c8091f0e8f03a
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-01 07:56:47 +0100

    create postService

--- backend/src/main/java/com/yrcode/_blog/services/PostServiceImpl.java
+++ backend/src/main/java/com/yrcode/_blog/services/PostServiceImpl.java
@@ -3,9 +3,10 @@ package com.yrcode._blog.services;
 import static java.util.stream.Collectors.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yrcode._blog.abstracts.PostService;
@@ -13,16 +14,20 @@ import com.yrcode._blog.dtos.post.PostCreateDTO;
 import com.yrcode._blog.dtos.post.PostDetailsDTO;
 import com.yrcode._blog.dtos.post.PostUpdateDTO;
 import com.yrcode._blog.entities.PostEntity;
+import com.yrcode._blog.entities.UserEntity;
 import com.yrcode._blog.repositories.PostRepo;
+import com.yrcode._blog.repositories.UserRepo;
+import com.yrcode._blog.security.SecurityUtils;
 import com.yrcode._blog.shared.CustomResponseException;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
+    @Autowired
     private PostRepo postRepo;
-    
+    @Autowired
+    private SecurityUtils securityUtils;
+    @Autowired
+    private UserRepo userRepo;
     @Override
     public PostDetailsDTO findOne(UUID id) {
         PostEntity post = postRepo.findById(id)
@@ -54,8 +59,28 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDetailsDTO createOne(PostCreateDTO data) {
+        UUID userId = securityUtils.getCurrentUserId();
+        UserEntity currentUser = userRepo.findById(userId)
+            .orElseThrow(()-> CustomResponseException.BadRequest("user not found!"));
         
-        return null;
+        PostEntity post = PostEntity.builder()
+            .title(data.title())
+            .content(data.content())
+            .mediaUrl(data.mediaUrl())
+            .mediaType(data.mediaType())
+            .userId(currentUser)
+            .build();
+        
+        PostEntity savedPost = postRepo.save(post);
+
+        return PostDetailsDTO.builder()
+            .id(savedPost.getId())
+            .title(savedPost.getTitle())
+            .content(savedPost.getContent())
+            .mediaUrl(savedPost.getMediaUrl())
+            .mediaType(savedPost.getMediaType())
+            .userId(savedPost.getUserId())
+            .build();
     }
 
     @Override
@@ -64,5 +89,12 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public void deleteOne(UUID id) {}
+    public void deleteOne(UUID id) {
+        UUID currentUserId = securityUtils.getCurrentUserId();
+        Optional<PostEntity> post = postRepo.findById(id);
+        if (post == null || post.get().getUserId() != currentUserId) {
+            throw CustomResponseException.BadRequest("Invalid post id!");
+        }
+        postRepo.deleteById(id);
+    }
 }
