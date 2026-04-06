a194c60a7c28c146685198d524660725f566aff2
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-06 15:54:03 +0100

    create post-card

---
+++ frontend/src/app/components/post-card/post-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-card',
+  imports: [],
+  templateUrl: './post-card.html',
+  styleUrl: './post-card.scss',
+})
+export class PostCard {}
