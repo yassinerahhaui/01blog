340ab5ac681f23b5b0fead33dcea6175256b77d1
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-06 15:49:30 +0100

    create user-card info component

---
+++ frontend/src/app/components/user-card/user-card.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Auth } from '../../core/services/auth';
+import { RouterLink } from "@angular/router";
+
+
+@Component({
+  selector: 'app-user-card',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './user-card.html',
+  styleUrl: './user-card.scss',
+})
+export class UserCard {
+  authService = inject(Auth);
+  user = signal(this.authService.currentUser());
+}
