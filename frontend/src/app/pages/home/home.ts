b60fbf77275e52af43ff5bea28b65b5f739eb60b
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-06 15:52:06 +0100

    update home page

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Slider } from '../../components/slider/slider';
+import { UserCard } from '../../components/user-card/user-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Slider,UserCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
