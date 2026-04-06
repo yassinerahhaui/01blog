5712d018c32e2c03db448b68399a39440ea581b1
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-06 15:48:31 +0100

    create a welcome slider

---
+++ frontend/src/app/components/slider/slider.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  imports: [],
+  templateUrl: './slider.html',
+  styleUrl: './slider.scss',
+})
+export class Slider {}
