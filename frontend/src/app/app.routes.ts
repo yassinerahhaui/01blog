510ffa68920fd76e5009427a89b72af3cd73dc73
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-07 17:50:26 +0100

    update router frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,10 +8,12 @@ import { notAuthGuard } from './core/guards/guest-guard';
 import { Profile } from './pages/profile/profile';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { adminGuard } from './core/guards/admin-guard';
+import { PostCreate } from './components/post-create/post-create';
 
 export const routes: Routes = [
   { path: '', component: Home, canActivate: [authGuard] },
   { path: 'profile', component: Profile, canActivate: [authGuard] },
+  { path: 'post/create', component: PostCreate, canActivate: [authGuard] },
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard, adminGuard] },
   { path: 'login', component: Login, canActivate: [notAuthGuard] },
   { path: 'register', component: Register, canActivate: [notAuthGuard] },
