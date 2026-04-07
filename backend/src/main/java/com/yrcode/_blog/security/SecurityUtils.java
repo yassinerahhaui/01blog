199060d164fd3f7e5b12e908956241971086176e
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-07 17:42:56 +0100

    fix getCurrentUserId

--- backend/src/main/java/com/yrcode/_blog/security/SecurityUtils.java
+++ backend/src/main/java/com/yrcode/_blog/security/SecurityUtils.java
@@ -2,23 +2,31 @@ package com.yrcode._blog.security;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import com.yrcode._blog.entities.UserEntity;
+import com.yrcode._blog.repositories.UserRepo;
 import com.yrcode._blog.shared.CustomResponseException;
 
 
 @Component
 public class SecurityUtils {
 
+    @Autowired
+    private UserRepo userRepo;
+
     public UUID getCurrentUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
-            UserEntity currentUser = (UserEntity) authentication.getPrincipal();
-            return currentUser.getId();
+            UserDetails currentUser = (UserDetails) authentication.getPrincipal();
+            UserEntity user = userRepo.findOneByUsername(currentUser.getUsername())
+                .orElseThrow(()-> CustomResponseException.BadRequest("invalid user!"));
+            return user.getId();
         }
 
         throw CustomResponseException.Unauthorized("you don't have permission!");
