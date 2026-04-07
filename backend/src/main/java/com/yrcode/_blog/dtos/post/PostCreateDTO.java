768d3e27221b8b302f453b2b37cf57f814b4a30b
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-07 17:40:59 +0100

    update post create dto

--- backend/src/main/java/com/yrcode/_blog/dtos/post/PostCreateDTO.java
+++ backend/src/main/java/com/yrcode/_blog/dtos/post/PostCreateDTO.java
@@ -1,11 +1,5 @@
 package com.yrcode._blog.dtos.post;
-
-import org.hibernate.validator.constraints.URL;
-
-import com.yrcode._blog.enums.MediaType;
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -17,12 +11,12 @@ public record PostCreateDTO(
 
     @NotBlank(message = "Post content cannot be empty.")
     @Size(max = 50000, message = "Post content is too long! (Max 50,000 characters)")
-    String content,
+    String content
     
-    @URL(message = "The media URL must be a valid and properly formatted link.")
-    @Size(max = 2083, message = "The media URL is too long.")
-    String mediaUrl,
+    // @URL(message = "The media URL must be a valid and properly formatted link.")
+    // @Size(max = 2083, message = "The media URL is too long.")
+    // String mediaUrl,
     
-    @NotNull(message = "Media type must be specified.")
-    MediaType mediaType
+    // @NotNull(message = "Media type must be specified.")
+    // MediaType mediaType
 ) {}
