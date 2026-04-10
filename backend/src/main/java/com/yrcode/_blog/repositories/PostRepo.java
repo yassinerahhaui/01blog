1a4e909894b9ef83dcd15ebe3634b019b4ee3fd0
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-10 12:22:59 +0100

    get postrs with userId with infinit scrole

--- backend/src/main/java/com/yrcode/_blog/repositories/PostRepo.java
+++ backend/src/main/java/com/yrcode/_blog/repositories/PostRepo.java
@@ -1,9 +1,20 @@
 package com.yrcode._blog.repositories;
 
-import java.util.UUID;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.yrcode._blog.entities.PostEntity;
 
-public interface PostRepo extends JpaRepository<PostEntity, UUID> {}
+import java.util.UUID;
+
+
+public interface PostRepo extends JpaRepository<PostEntity, UUID> {
+
+    // مسحنا الميثودات القدام وخلينا غير هادي لي خدامة بـ @Query
+    @Query("SELECT p FROM PostEntity p WHERE p.userId = :userId ORDER BY p.createdAt DESC, p.lastUpdate DESC")
+    Slice<PostEntity> findPostsByUserOrdered(@Param("userId") UUID userId, Pageable pageable);
+    
+}
