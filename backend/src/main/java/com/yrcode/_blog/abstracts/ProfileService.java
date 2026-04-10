6940a6fddf4d6f7c19e37d3532534880ac10e6fb
yrahhaou <yassinerahhaoui12@gmail.com>
2026-04-10 12:39:59 +0100

    create profile service interface

---
+++ backend/src/main/java/com/yrcode/_blog/abstracts/ProfileService.java
@@ -0,0 +1,9 @@
+package com.yrcode._blog.abstracts;
+
+import java.util.UUID;
+import org.springframework.data.domain.Slice;
+import com.yrcode._blog.dtos.post.PostDetailsDTO;
+
+public interface ProfileService {
+    Slice<PostDetailsDTO> getProfilePosts(UUID userId, int page);
+}
